c9cf69584e4e2e096a3bee54d98841064f06fd6a
SettlementApp <aki01191017@gmail.com>
2021-10-27 12:32:46 +0900

    Initial Commit

---
+++ BarcodeScanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarcodeScanApp
+//
+//  Created by 柿沼儀揚 on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
